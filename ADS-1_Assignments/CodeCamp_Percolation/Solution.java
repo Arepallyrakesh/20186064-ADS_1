3118fa2adf61913f99815e431d7a0f4bde1d4fc8
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-09-25 22:06:54 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- ADS-1_Assignments/CodeCamp_Percolation/Solution.java
+++ ADS-1_Assignments/CodeCamp_Percolation/Solution.java
@@ -14,7 +14,7 @@ class Solution {
         Scanner sc = new Scanner(System.in);
         int input = Integer.parseInt(sc.nextLine());
         percolate obj = new percolate(input);
-        while(sc.hasNextLine()) {
+        while (sc.hasNextLine()) {
             String[] inputs = sc.nextLine().split(" ");
             obj.open(Integer.parseInt(inputs[0]) - 1, Integer.parseInt(inputs[1]) - 1);
         }
@@ -30,6 +30,12 @@ class percolate {
     boolean[][] grid;
     WeightedQuickUnionUF obj;
     int size;
+
+    /**
+     * Constructs the object.
+     *
+     * @param      size  The size
+     */
     percolate(final int size) {
         this.size = size;
         grid = new boolean[size][size];
@@ -48,7 +54,7 @@ class percolate {
         }
         grid[row][column] = true;
         if (row == 0) {
-            obj.union(convert(row, column), size*size);
+            obj.union(convert(row, column), size * size);
         }
         if (row == size - 1) {
             obj.union(convert(row, column), size * size + 1);
@@ -56,10 +62,10 @@ class percolate {
         if (row < size - 1 && grid[row + 1][column]) { //bottom element
             obj.union(convert(row, column), convert(row + 1 , column));
         }
-        if (row > 0 && grid[row-1][column]) { //top element
+        if (row > 0 && grid[row - 1][column]) { //top element
             obj.union(convert(row, column), convert(row - 1 , column));
         }
-        if (column > 0 && grid[row][column-1]) { //left element
+        if (column > 0 && grid[row][column - 1]) { //left element
             obj.union(convert(row, column), convert(row, column - 1));
         }
         if (column < size - 1 && grid[row][column + 1]) { //right element
