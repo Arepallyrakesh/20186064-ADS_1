410c9c6ef9498cee0c3ac5dcd63a628791c62acb
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-09-25 21:45:37 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- ADS-1_Assignments/CodeCamp_Percolation/Solution.java
+++ ADS-1_Assignments/CodeCamp_Percolation/Solution.java
@@ -21,33 +21,33 @@ class percolate{
         obj = new WeightedQuickUnionUF(size * size + 2);
     }
     public void open(int row, int column){
-        if(grid[row][column]){
+        if (grid[row][column]){
             return;
         }
         grid[row][column] = true;
-        if(row == 0){
+        if (row == 0){
             obj.union(convert(row,column), size*size);
         }
-        if(row == size-1){
-            obj.union(convert(row, column), size*size+1);
+        if (row == size-1){
+            obj.union(convert(row, column), size * size + 1);
         }
-        if(row < size - 1 && grid[row+1][column]){ //bottom element
-            obj.union(convert(row,column), convert(row+1, column));
+        if (row < size - 1 && grid[row + 1][column]){ //bottom element
+            obj.union(convert(row,column), convert(row + 1, column));
         }
-        if(row > 0 && grid[row-1][column]){ //top element
-            obj.union(convert(row,column), convert(row-1, column));
+        if (row > 0 && grid[row-1][column]){ //top element
+            obj.union(convert(row,column), convert(row - 1, column));
         }
-        if(column > 0 && grid[row][column-1]){ //left element
-            obj.union(convert(row,column), convert(row, column-1));
+        if (column > 0 && grid[row][column-1]){ //left element
+            obj.union(convert(row,column), convert(row, column - 1));
         }
-        if(column < size - 1 && grid[row][column+1]){ //right element
-            obj.union(convert(row,column), convert(row, column+1));
+        if (column < size - 1 && grid[row][column + 1]){ //right element
+            obj.union(convert(row,column), convert(row, column + 1));
         }
 
 
     }
     public boolean ispercolate(){
-        return obj.connected(size * size, size*size+1);
+        return obj.connected(size * size, size * size + 1);
     }
 
     public int convert(int i, int j){
