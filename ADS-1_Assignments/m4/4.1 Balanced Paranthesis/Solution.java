7d2dac2a34b72bad85cb3cead45aa88f04d259ab
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-09-27 17:53:40 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1_Assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ ADS-1_Assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -1,55 +1,117 @@
 import java.util.Scanner;
-import java.util.Arrays;
-class LinkList{
-	private Node firstelem = null;
-	LinkList() {
-	}
-	class Node{
-		String element;
-		Node nextaddress;
-	}
-	boolean isempty(){
-		return firstelem == null;
-	}
-	void push(String item) {
-		Node oldfirst = firstelem;
-		firstelem = new Node();
-		firstelem.element = item;
-		firstelem.nextaddress = oldfirst;
-	}
-	String pop(String str) {
-		if (isempty() || str != firstelem.element) {
-			return null;
-		}
-		String popitem = firstelem.element;
-		firstelem = firstelem.nextaddress;
-		return popitem;
-	}
-	boolean isbalance(String[] s) {
-		int countopen = 0;
-		int countclosed = 0;
-		for (String inp: s) {
-			if (inp.equals(")") || inp.equals("]") || inp.equals("}")) {
-				countopen++;
-			} else if (inp.equals("(") || inp.equals("[") || inp.equals("{")) {
-				countclosed++;
-			}
-		}
-		return countopen == countclosed;
-	}
+/**
+ * Class for LinkedList.
+ */
+class LinkedList {
+    /**
+     * {Initializing Nodeclass Firstelement}.
+     */
+    private Node firstelement = null;
+    /**
+     * Class for node.
+     */
+    private class Node {
+        /**
+         * {initializing char item}.
+         */
+        private char item;
+        /**
+         * {initializing nextAddress}.
+         */
+        private Node nextAddress;
+    }
+
+    /**
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public char top() {
+        return firstelement.item;
+    }
+
+
+    /**
+     * Determines if empty.
+     *
+     * @return     True if empty, False otherwise.
+     */
+    public boolean isEmpty() {
+        return firstelement == null;
+    }
+    /**
+     * { function_description }
+     *
+     * @param      item  The item
+     */
+    public void push(final char item) {
+        Node oldfirst = firstelement;
+        firstelement = new Node();
+        firstelement.item = item;
+        firstelement.nextAddress = oldfirst;
+    }
+    /**
+     * { function_description }.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public char pop() {
+        char item = firstelement.item;
+        firstelement = firstelement.nextAddress;
+        return item;
+    }
 }
-public class Solution{
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int input = scan.nextInt();
-		LinkList ll = new LinkList();
-		while(input > 0) {
-			String[] s = scan.nextLine().split("");
-			if(ll.isbalance(s)) {
-				
-			}
-			input--;
-		}
-		System.out.println(ll.isempty());
-	}
+
+public final class Solution {
+    /**
+     * Constructs the object of solution.
+     */
+    private Solution() {
+
+    }
+    /**
+     * main function is here.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int size = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < size; i++) {
+            String line = sc.next();
+            if(checkBalancedParanthesis(line)){
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+
+        }
+    }
+    public static boolean checkBalancedParanthesis(final String s) {
+        LinkedList obj = new LinkedList();
+        int sLength = s.length();
+        for (int i = 0; i < sLength; i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                obj.push(ch);
+            }
+            else {
+                if(obj.isEmpty()) {
+                    return false;
+                }
+                if (ch == ')' && obj.top() == '(') {
+                obj.pop();
+                } else if (ch == '}' && obj.top() == '{') {
+                    obj.pop();
+                } else if (ch == ']' && obj.top() == '[') {
+                    obj.pop();
+                } else {
+                    return false;
+                }
+            }
+
+        }
+        return obj.isEmpty();
+    }
 }
