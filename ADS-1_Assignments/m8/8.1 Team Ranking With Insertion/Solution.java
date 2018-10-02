5d81656ff58874684f1e14bf1cc362ede184d290
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-10-02 23:42:05 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

--- ADS-1_Assignments/m8/8.1 Team Ranking With Insertion/Solution.java
+++ ADS-1_Assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -1,108 +1,36 @@
 import java.util.Scanner;
-import java.util.*;
-class Team {
-	String teamname;
-	int wins;
-	int loses;
-	int draw;
-	Team() { }
-	Team(String[] inp) {
-		teamname = inp[0];
-		wins = Integer.parseInt(inp[1]);
-		loses = Integer.parseInt(inp[2]);
-		draw = Integer.parseInt(inp[3]);
-	}
-	String geteam() {
-		return teamname;
-	}
-	void seteam(String tm) {
-		teamname = tm;
-	}
-
-	int getwins() {
-		return wins;
-	}
-	void sewins(int win)  {
-		wins = win;
-	}
-
-	int getloses() {
-		return loses;
-	}
-	void seloses(int lose)  {
-		loses = lose;
-	}
-
-	int getdraw() {
-		return draw;
-	}
-	void sedraw(int dr)  {
-		draw = dr;
-	}
-	public String toString() {
-		String str = "";
-		str = geteam();
-		return str;
-	}
-}
- class SelectionSort {
- 	Team[] team;
- 	int size;
- 	SelectionSort(Team[] tm, int sz) {
- 		team = tm;
- 		size = sz;
- 	}
- 	Team[] sort() {
- 		for (int i = 0; i < size - 1; i++) {
- 			int max = i;
- 			for (int j = i+1; j < size; j++) {
-	 			if (team[j].getwins() > team[max].getwins()) {
-	 				max = j;
-	 			} else if (team[j].getwins() == team[max].getwins()) {
-	 				if (team[j].getloses() < team[max].getloses()) {
-	 					max = j;
-	 				}
-	 				if (team[j].getloses() == team[max].getloses()) {
-	 					// System.out.println(team[max].geteam()+":team[max]----------------team[j]:"+team[j].geteam());
-		 				if (team[j].getdraw() > team[max].getdraw()) {
-		 					// System.out.println("****");
-		 					max = j;
-		 					}
-		 				}
-	 			}
- 			}
- 			Team temp = team[i];
- 			team[i] = team[max];
- 			team[max] = temp;
- 		}
- 		return team;
- 	}
-
- }
-
-class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Team[] tm = new Team[10];
-		int size = 0;
-        while (sc.hasNextLine()){
-        	// System.out.println("in while");
-	        String input = sc.nextLine();
-			if (input.equals("")) {
-				break;
-			}
-	        // System.out.println(input+":input");
-	        String[] tokens =  input.split(",");
-			tm[size++] = new Team(tokens);
-    	}
-    	// System.out.println("out while:");
-    	SelectionSort select = new SelectionSort(tm, size);
-    	tm = select.sort();
-    	String str = "";
-    	for (int i  = 0; i < size-1; i ++) {
-    		str += tm[i].toString() + ",";
-    	}
-    	str += tm[size-1].toString();
-    	System.out.println(str);
-	}
+/**.
+ * Class for Solution.
+ */
+public final class Solution {
+    /**.
+     * Magic Number (check style).
+     */
+    private static final int TEN = 10;
+    /**.
+     * Magic Number (check style).
+     */
+    private static final int THREE = 3;
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+/**
+ * Main function to handle test cases and deliver required output.
+ *
+ * @param      args  The arguments
+ */
+    public static void main(final String[] args) {
+        Team[] cricketteams = new Team[TEN];
+        int size = 0;
+        Scanner scan = new Scanner(System.in);
+        Leaderboard t = new Leaderboard();
+        while (scan.hasNext()) {
+            String[] line = scan.nextLine().split(",");
+            cricketteams[size++] = new Team(line[0], Integer.parseInt(line[1]),
+                     Integer.parseInt(line[2]), Integer.parseInt(line[THREE]));
+        }
+        System.out.println(t.leaderboard(cricketteams, size));
+    }
 }
