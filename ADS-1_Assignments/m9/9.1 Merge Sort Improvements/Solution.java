45f50bdcd347be7734fe0a45c7de4ba56583accf
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-10-03 23:24:29 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ ADS-1_Assignments/m9/9.1 Merge Sort Improvements/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+class Solution {
+
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Merge ms = new Merge();
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNextLine()) {
+            String tokens = scan.nextLine();
+            String[] lines = tokens.split(",");
+            ms.sort(lines);
+            System.out.println(ms.show(lines));
+            System.out.println();
+
+        }
+    }
+}
