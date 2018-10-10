c183ac94d2c24d21a7adaf57068b86102f1e5af5
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-10-10 22:46:02 +0530

    "ads1_m15_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1_Assignments/m15/15.1 BinarySearchSymbolTable/Solution.java
+++ ADS-1_Assignments/m15/15.1 BinarySearchSymbolTable/Solution.java
@@ -1,39 +1,48 @@
-import java.util.*;
+import java.util.Scanner;
+
+/**
+ * Class for solution.
+ */
 class Solution {
-	Solution() {
+    /**
+     * Constructs the object.
+     */
+    Solution() {
 
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String line[] = sc.nextLine().split(" ");
-		BinarySearchST<String, Integer> obj = new BinarySearchST<String, Integer>();
-		for (int i = 0; i<line.length; i++) {
-			obj.put(line[i],i);
-		}
-		/*System.out.println(line);*/
-		 
-    
-		/*System.out.println(Arrays.toString(line1));*/
+    }
 
-		while (sc.hasNext()) {
-			String[] token = sc.nextLine().split(" ");
-			/*System.out.println(token);*/
-			if (token[0].equals("deleteMin")) {
-				obj.deleteMin();
-			} else if (token[0].equals("max")) {
-				System.out.println(obj.max());
-			} else if (token[0].equals("floor")) {
-				System.out.println(obj.floor(token[1]));
-			} else if (token[0].equals("rank")) {
-				System.out.println(obj.rank(token[1]));
-			} else if (token[0].equals("contains")) {
-				System.out.println(obj.contains(token[1]));
-			} else if (token[0].equals("get")) {
-				System.out.println(obj.get(token[1]));
-			} else if (token[0].equals("keys")) {
-				obj.keys();
-			}
-		}
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line[] = sc.nextLine().split(" ");
+        BinarySearchST<String, Integer> obj = new BinarySearchST<String, Integer>();
+        for (int i = 0; i<line.length; i++) {
+            obj.put(line[i],i);
+        }
+        /*System.out.println(line);*/
+        while (sc.hasNext()) {
+            String[] token = sc.nextLine().split(" ");
+            /*System.out.println(token);*/
+            if (token[0].equals("deleteMin")) {
+                obj.deleteMin();
+            } else if (token[0].equals("max")) {
+                System.out.println(obj.max());
+            } else if (token[0].equals("floor")) {
+                System.out.println(obj.floor(token[1]));
+            } else if (token[0].equals("rank")) {
+                System.out.println(obj.rank(token[1]));
+            } else if (token[0].equals("contains")) {
+                System.out.println(obj.contains(token[1]));
+            } else if (token[0].equals("get")) {
+                System.out.println(obj.get(token[1]));
+            } else if (token[0].equals("keys")) {
+                obj.keys();
+            }
+        }
 
-	}
+    }
 }
