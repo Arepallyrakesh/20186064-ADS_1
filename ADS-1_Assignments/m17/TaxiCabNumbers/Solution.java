c6089d223afa0a06f7a601c439331d0c4b9ea286
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-10-12 22:24:38 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1_Assignments/m17/TaxiCabNumbers/Solution.java
+++ ADS-1_Assignments/m17/TaxiCabNumbers/Solution.java
@@ -1,8 +1,22 @@
-import java.util.*;
-class Solution {
-	public static void main(String[] args) {
-		CubeSum obj = new CubeSum();
-		Scanner sc= new Scanner(System.in);
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution(){
+
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        CubeSum obj = new CubeSum();
+        Scanner sc = new Scanner(System.in);
         String[] n = sc.nextLine().split(" ");
         int n1 = Integer.parseInt(n[0]);
         int n2 = Integer.parseInt(n[1]);
