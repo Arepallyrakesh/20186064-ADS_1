ca681cb18e72669c7469cc709ca39826528497e0
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-10-23 16:40:57 +0530

    "ads1_m13_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-1_Assignments/m13/13.1 isMinHeap/MinPQ.java
+++ ADS-1_Assignments/m13/13.1 isMinHeap/MinPQ.java
@@ -32,80 +32,6 @@ public class  MinPQ<Key> {
 	public  MinPQ() {
 		this(1);
 	}
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      keys  The keys.
-	 */
-	public  MinPQ(Key[] keys) {
-		n = keys.length;
-		pq = (Key[]) new Object[keys.length + 1];
-		for (int i = 0; i < n; i++)
-			pq[i + 1] = keys[i];
-		assert isMinHeap();
-	}
-	/**
-	 * Determines if empty.
-	 *
-	 * @return     True if empty, False otherwise.
-	 */
-	public boolean isEmpty() {
-		return n == 0;
-	}
-	/**
-	 * { size of key array }.
-	 *
-	 * @return     { void }.
-	 */
-	public int size() {
-		return n;
-	}
-	/**
-	 * resizing array.
-	 *
-	 * @param      capacity  The capacity.
-	 */
-	public void resize(int capacity) {
-		assert capacity > n;
-		Key[] temp = (Key[]) new Object[capacity];
-		for (int i = 1; i <= n; i++) {
-			temp[i] = pq[i];
-		}
-		pq = temp;
-	}
-	/**
-	 * inserting key in priotiy queue.
-	 *
-	 * @param      x     {elements of the queue}.
-	 */
-	public void insert(Key x) {
-		if (n == pq.length - 1) resize(2 * pq.length);
-
-		pq[++n] = x;
-		swim(n);
-		assert isMinHeap();
-	}
-	/**
-	 * show priotiy queue.
-	 *
-	 * @return     {String}.
-	 */
-	public String show() {
-		String str = "";
-		for (int i = 0; i < pq.length; i++) {
-			if (pq[i] != null) {
-				str += pq[i];
-			}
-		}
-		return str;
-	}
-
-	public void swim(int k) {
-		while (k > 1 && greater(k / 2, k)) {
-			exch(k, k / 2);
-			k = k / 2;
-		}
-	}
 	public boolean greater(int i, int j) {
 		if (comparator == null) {
 			return ((Comparable<Key>) pq[i]).compareTo(pq[j]) > 0;
@@ -114,11 +40,7 @@ public class  MinPQ<Key> {
 		}
 	}
 
-	public void exch(int i, int j) {
-		Key swap = pq[i];
-		pq[i] = pq[j];
-		pq[j] = swap;
-	}
+	
 
 	public boolean isMinHeap() {
 		return isMinHeap(1);
