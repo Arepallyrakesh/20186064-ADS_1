41d7612494d631f1fb5fe79c7509d9994d0a8c07
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-10-23 16:09:36 +0530

    "ads1_m13_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-1_Assignments/m13/13.1 isMinHeap/Solution.java
+++ ADS-1_Assignments/m13/13.1 isMinHeap/Solution.java
@@ -1,23 +1,40 @@
 import java.util.Scanner;
+/**
+ * Class for minheap.
+ */
+class Minheap {
+    /**
+     * Determines if heap.
+     *  Best case: O(1)
+     *  worst case :O(n)
+     *  Average case: O(n)
+     *
+     * @param      a     { parameter_description }
+     *
+     * @return     True if heap, False otherwise.
+     */
+    public boolean isHeap(final Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            if (2 * i + 1 < a.length && 2 * i + 2 < a.length) {
+                if (a[i].compareTo(a[2 * i + 1]) > 0
+                 || a[i].compareTo(a[2 * i + 2]) > 0) {
+                    return false;
+                }
+            }
 
+        }
+        return true;
+    }
+}
 /**
  * Class for solution.
  */
-public final class Solution {
+final class Solution {
     /**
      * Constructs the object.
      */
-    private Solution() { }
-    /**
-     * compare.
-     *
-     * @param      pq    priority queue.
-     * @param      str   The string.
-     *
-     * @return     {compare it}.
-     */
-    public static boolean compare(final String pq, final String str) {
-        return pq.equals(str);
+    private Solution() {
+
     }
     /**
      * main.
@@ -25,96 +42,55 @@ public final class Solution {
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
+        int size;
         Scanner sc = new Scanner(System.in);
-        String chk = sc.nextLine();
-        switch (chk) {
-            case "String" :
-            int num = sc.nextInt();
-            sc.nextLine();
-            while (sc.hasNext()) {
-                String line = sc.nextLine();
-                String str = "";
-                MinPQ<String> pq = new MinPQ<String>();
-                if (line.length() != 0) {
-                    String[] token = line.split(",");
-                    for (int j = 0; j < token.length; j++) {
-                        pq.insert(token[j]);
-                        str += token[j];
+        String type = sc.nextLine();
+        int nooftestcases = sc.nextInt();
+        Minheap m = new Minheap();
+        sc.nextLine();
+        for (int i = 0; i < nooftestcases; i++) {
+            String line = sc.nextLine();
+            if (line.length() == 0) {
+                System.out.println("false");
+            } else {
+                String[] input = line.split(",");
+                switch (type) {
+                case "String":
+                    size  = 0;
+                    String[] stringheap = new String[input.length];
+                    for (int j = 0; j < input.length; j++) {
+                        stringheap[size++] = input[j];
                     }
-                    System.out.println(compare(pq.show(), str));
-                } else {
-                    System.out.println("false");
-                }
-            }
-            break;
-            case "Float" :
-            int numf = sc.nextInt();
-            sc.nextLine();
-            while (numf > 0) {
-                String line = sc.nextLine();
-                String str = "";
-                MinPQ<Float> pq = new MinPQ<Float>();
-                if (line.length() != 0) {
-                    String[] token = line.split(",");
-                    Float[] floatarray = new Float[token.length];
-                    for (int j = 0; j < token.length; j++) {
-                        floatarray[j] = Float.parseFloat(token[j]);
-                        pq.insert(floatarray[j]);
-                        str += floatarray[j];
+                    System.out.println(m.isHeap(stringheap));
+                    break;
+                case "Integer":
+                    size = 0;
+                    Integer[] intheap = new Integer[input.length];
+                    for (int j = 0; j < input.length; j++) {
+                        intheap[size++] = Integer.parseInt(input[j]);
                     }
-                    System.out.println(compare(pq.show(), str));
-                } else {
-                    System.out.println("false");
-                }
-                numf--;
-            }
-
-            break;
-            case "Double" :
-            int numD = sc.nextInt();
-            sc.nextLine();
-            while (sc.hasNext()) {
-                String line = sc.nextLine();
-                String str = "";
-                MinPQ<Double> pq = new MinPQ<Double>();
-                if (line.length() != 0) {
-                    String[] token = line.split(",");
-                    Double[] doublearray = new Double[token.length];
-                    for (int k = 0; k < token.length; k++) {
-                        doublearray[k] = Double.parseDouble(token[k]);
-                        pq.insert(doublearray[k]);
-                        str += doublearray[k];
-                        }
-                    System.out.println(compare(pq.show(), str));
-                } else {
-                    System.out.println("false");
-                }
-            }
-            break;
-            case "Integer" :
-            int numI = sc.nextInt();
-            sc.nextLine();
-            while (sc.hasNext()) {
-                String line = sc.nextLine();
-                String str = "";
-                MinPQ<Integer> pq = new MinPQ<Integer>();
-                if (line.length() != 0) {
-                    String[] token = line.split(",");
-                    Integer[] integerarray = new Integer[token.length];
-                    for (int k = 0; k < token.length; k++) {
-                        integerarray[k] = Integer.parseInt(token[k]);
-                        pq.insert(integerarray[k]);
-                        str += integerarray[k];
+                    System.out.println(m.isHeap(intheap));
+                    break;
+                case "Float":
+                    size = 0;
+                    Float[] floatheap = new Float[input.length];
+                    for (int j = 0; j < input.length; j++) {
+                        floatheap[size++] = Float.parseFloat(input[j]);
                     }
-                    System.out.println(compare(pq.show(), str));
-                } else {
-                    System.out.println("false");
+                    System.out.println(m.isHeap(floatheap));
+                    break;
+                case "Double":
+                    size = 0;
+                    Double[] doubleheap = new Double[input.length];
+                    for (int j = 0; j < input.length; j++) {
+                        doubleheap[size++] = Double.parseDouble(input[j]);
+                    }
+                    System.out.println(m.isHeap(doubleheap));
+                    break;
+                default:
+                    break;
                 }
             }
-            break;
-        default:
-            break;
         }
-
     }
 }
