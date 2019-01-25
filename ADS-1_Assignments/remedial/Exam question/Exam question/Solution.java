d1af008d0316db946dfd83860d84760145feba5b
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2019-01-25 12:22:29 +0530

    "ADS-1 remedial-2: 0/10 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1_Assignments/remedial/Exam question/Exam question/Solution.java
+++ ADS-1_Assignments/remedial/Exam question/Exam question/Solution.java
@@ -11,38 +11,28 @@ class Solution {
 		
 		Scanner s = new Scanner(System.in);
 		String si = s.next();
-
-
-
-		File dir = new File("D:/20186064-ADS_1/ADS-1_Assignments/remedial/Exam question/Exam question/Files");
-
-		for (File file : dir.listFiles(new FilenameFilter() {
-    public boolean accept(File dir, String name) {
-        return name.toLowerCase().endsWith(".txt");
-
-    }
-})) {
 			ArrayList<Integer> arrli = new ArrayList<Integer>();
-			String str = "";
+			
 			String s4= "";
 			int count = 0;
 			int count1 = -1;
-			try {
-	    	s = new Scanner(file);
-	    	 while (s.hasNextLine()) 
-	    	 	str += s.nextLine() + "\n";
-	    	 count1++;
-      			// System.out.println(s.nextLine());
-	    	
-	    	 String[] str1 = str.split(" ");
+			
 	    	 
       			//System.out.println(Arrays.toString(str1)); 
       			String Name = si;
 				int index = -1;
 				int i = 0;
 				String s5 = "";
-				for (i=0;i<str1.length;i++) {
-				    if (str1[i].equals(Name)) {
+				try{
+
+
+		for (int a = 0; a <= 8; a++)  {
+			Scanner sc = new Scanner (new File("Files/"+a+".txt"));
+			String[] str = sc.nextLine().split(" ");
+	    	 count1++;
+
+			for (i=0;i<str.length;i++) {
+				    if (str[i].equals(Name)) {
 				        index = i;
 				        count++;
 				       arrli.add(i);
@@ -54,16 +44,16 @@ class Solution {
 				    System.out.println(count1+ ", " +count+", " +arrli);
 				}
 				//System.out.println(arrli); 
-				
-
-			
+				}
 				//System.out.println(ss.get("moana"));
 				  
-	}
+	
+}
+				 catch (Exception e) {
+         
+         System.out.println("no file found");
+      }
 
-		catch( Exception e) {
-		System.out.println("file not found");
-		}
 			//System.out.println(str);
 	
 
@@ -71,4 +61,3 @@ class Solution {
 	}
 
 	}
-}
--- ADS-1_Assignments/remedial/Exam question/Exam question/test.java
+++ ADS-1_Assignments/remedial/Exam question/Exam question/test.java
@@ -1,17 +1,62 @@
 import java.io.File; 
 import java.util.Scanner; 
+import java.util.Arrays;
 public class test 
 { 
   public static void main(String[] args) throws Exception 
   { 
     // pass the path to the file as a parameter 
    
-          for (int i=0; i < 8 ; i++ ) {
-    	Scanner sc = new Scanner(new File("D:/20186064-ADS_1/ADS-1_Assignments/remedial/Exam question/Exam question/Files"+i)); 
- 
-  
-      System.out.println(i + "    "+sc.nextLine()); 
-    	
-    }
+    for (int i = 0; i <= 8; i++) {
+		Scanner sc = new Scanner (new File("Files/"+i+".txt"));
+		String[] str = sc.nextLine().split(" ");
+		//System.out.println(i + Arrays.toString(str));
+		ArrayList<Integer> arrli = new ArrayList<Integer>();
+				String str = "";
+				String s4= "";
+				int count = 0;
+				int count1 = -1;
+				try {
+		    	s = new Scanner(file);
+		    	 while (s.hasNextLine()) 
+		    	 	str += s.nextLine() + "\n";
+	      			// System.out.println(s.nextLine());
+		    	
+		    	 String[] str1 = str.split(" ");
+		    	 count1++;
+		    	 
+	      			//System.out.println(Arrays.toString(str1)); 
+	      			String Name = si;
+					int index = -1;
+					int i = 0;
+					String s5 = "";
+					for (i=0;i<str1.length;i++) {
+					    if (str1[i].equals(Name)) {
+					        index = i;
+					        count++;
+				       arrli.add(i);
+				   	//	System.out.print(s4);
+				    }
+				}
+				if (count > 0) {
+					
+				    System.out.println(count1+ ", " +count+", " +arrli);
+				}
+				//System.out.println(arrli); 
+				
+
+			
+				//System.out.println(ss.get("moana"));
+				  
+	}
+
+		catch( Exception e) {
+		System.out.println("file not found");
+		}
+			//System.out.println(str);
+	
+
+	    	s.close();
+	}
+}
   } 
-} 
